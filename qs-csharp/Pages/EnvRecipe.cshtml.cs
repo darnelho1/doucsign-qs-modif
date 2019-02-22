fba3c173594d5bec58cd8e7d15b9d1847690c99e
Darnell Holder <darnelho1@gmail.com>
2019-02-22 13:26:59 -0800

    Corrected names and titles started new method.

---
+++ qs-csharp/Pages/EnvRecipe.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DocuSign.eSign.Api;
+using DocuSign.eSign.Client;
+using DocuSign.eSign.Model;
+using static DocuSign.eSign.Api.EnvelopesApi;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace qs_csharp.Pages
+{
+    public class EnvRecipeModel : PageModel
+    {
+        // Constants need to be set:
+        private const string accessToken = "eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQkAAAABAAUABwCA-0DJAZnWSAgAgDtk10SZ1kgCAJ8giZesq0hAr93A-qwGx1gVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MACAKy62AZnWSDcAYEcIb3TtBU6HTBBHVB7pfg.XzvS6NvNsRnLuleMebjoiFmtzO2bP9V-G9DkccFqW6f982zeVOF_9VtFt07Wi4C7N5SchYqDo5kdQEes-T9jmDLSo8ZyNrfHKg3hSRPwiJG1chaeCYTt_VChFHfDrxkuwvdiebCfAvEimbI3UTzuH9iD8FCpV3Wr1fiE-u_TVLIoTzjC31IGu6KJWdUMbSgmLjDy5Q7Qkjq3Nbd-O4_AlhIoE0xd7JiSxxP-8uZ22KJ0QNr1PoovGcm3uWu3JUDXNMj0qFGrzi6HPs4uHWLgVnY3Vp_EfQR1s8LG_sG0Wz1jwBL8vxZs9-8HlP4N_AIXygCGYXp58DR0uIm8EemA4g";
+        private const string accountId = "3094776";
+        private const string envelopeId = "f23acc3f-0f03-4130-8b3d-716d536ad776";
+        //private const int envelopesAgeDays = -10;
+
+        // Additional constants
+        private const string basePath = "https://demo.docusign.net/restapi";
+
+        public void OnGet()
+        {
+            ApiClient apiClient = new ApiClient(basePath);
+            apiClient.Configuration.AddDefaultHeader("Authorization", "Bearer " + accessToken);
+            EnvelopesApi envelopesApi = new EnvelopesApi(apiClient.Configuration);
+            Recipients recips = envelopesApi.ListRecipients(accountId, envelopeId);
+
+
+            // Prettyprint the results
+            string json = JsonConvert.SerializeObject(recips);
+            string jsonFormatted = JValue.Parse(json).ToString(Formatting.Indented);
+            ViewData["EnvelopeRecipients:"] = jsonFormatted;
+            Console.WriteLine(jsonFormatted);
+
+            return;
+        }
+    }
+}
--- qs-csharp/Pages/dlEnvelope.cshtml.cs
+++ qs-csharp/Pages/dlEnvelope.cshtml.cs
@@ -9,32 +9,37 @@ using Newtonsoft.Json.Linq;
 
 namespace qs_csharp.Pages
 {
-    public class EnvStatusModel : PageModel
+    public class dlEnvelopeModel : PageModel
     {
         // Constants need to be set:
         private const string accessToken = "eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQkAAAABAAUABwCAcnfylJjWSAgAgLKaANiY1kgCAJ8giZesq0hAr93A-qwGx1gVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4MACA8wPcYZjWSDcAYEcIb3TtBU6HTBBHVB7pfg.aJn81whnuKK2SzA7036Mz8TyH0q_bIWpGdsW2gzkWMmvJDO1Vt-bjarmjVtJQ5j1p-K249I-UVOD53H9nwxJzKSbedO9cDOMSL53vKQI79FwtwN_ctekIAcZriIwGQyuoHPp5U2wyO8uCxV5x_SAUKfoiQ0LI6X5ul3M43K-FaBSu2lMNIfPOAaO42AvzlZNCkq2J2ZiLczRJa-3R997KS7nJbr7M7DPpBPgZd-I2F-dWRxs6mjlWMbRX4uRd4D4Pm8o6fhdqz70MP1XJUH18SXOrGYtfI4p1pWE68x88VxuFGvNeAI0NzfUtsIYsUIAIvXp-MNCEBWVZiZtY3MFjQ";
         private const string accountId = "3094776";
-        private const string envelopeId = "f23acc3f-0f03-4130-8b3d-716d536ad776";
-        //private const int envelopesAgeDays = -10;
+        private const int envelopesAgeDays = -10;
 
         // Additional constants
         private const string basePath = "https://demo.docusign.net/restapi";
 
         public void OnGet()
         {
+            // List the user's envelopes created in the last 10 days
+            // 1. Create request options
+            // 2. Use the SDK to list the envelopes
+
+            // 1. Create request options
+            ListStatusChangesOptions options = new ListStatusChangesOptions();
+            DateTime date = DateTime.Now.AddDays(envelopesAgeDays);
+            options.fromDate = date.ToString("yyyy/MM/dd");
+
+            // 2. Use the SDK to list the envelopes
             ApiClient apiClient = new ApiClient(basePath);
             apiClient.Configuration.AddDefaultHeader("Authorization", "Bearer " + accessToken);
             EnvelopesApi envelopesApi = new EnvelopesApi(apiClient.Configuration);
-            Recipients recips = envelopesApi.ListRecipients(accountId, envelopeId);
-
-            // print the JSON response
-            Console.WriteLine("Recipients:\n{0}", JsonConvert.SerializeObject(recips));
+            EnvelopesInformation results = envelopesApi.ListStatusChanges(accountId, options);
 
             // Prettyprint the results
-            string json = JsonConvert.SerializeObject(recips);
+            string json = JsonConvert.SerializeObject(results);
             string jsonFormatted = JValue.Parse(json).ToString(Formatting.Indented);
-            ViewData["EnvelopeInformation:"] = jsonFormatted;
-            Console.WriteLine(jsonFormatted);
+            ViewData["results"] = jsonFormatted;
 
             return;
         }
